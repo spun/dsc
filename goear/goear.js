f99b52cf7ee43d4cc662fcb5f8bd15f756a80f80
spun <neoappengine@gmail.com>
2013-07-30 16:49:24 -0400

    Corregido problema con el nuevo diseño de goear.

--- goear/goear.js
+++ goear/goear.js
@@ -1,7 +1,8 @@
-/*global jQuery: false */
+/*global ActiveXObject: false */
+/*jslint browser: true, regexp: true */
 
 function setHtml5Player(url) {
-    "use strict";
+    'use strict';
     var audio, dsc_audio = document.getElementById('dsc_audio');
 
     if (dsc_audio === null) {
@@ -19,7 +20,7 @@ function setHtml5Player(url) {
 }
 
 function getId() {
-    "use strict";
+    'use strict';
     var id, url, splitUrl, count, i;
     url = location.href;
     splitUrl = url.split('/');
@@ -34,60 +35,42 @@ function getId() {
 }
 
 function getAudioData(audioId, callback) {
-    "use strict";
-    jQuery.ajax({
-        type: 'GET',
-        url: 'http://www.goear.com/tracker758.php?f=' + audioId,
-        dataType: 'html',
-        success: function (texto) {
-            callback(texto);
+    'use strict';
+    var xmlhttp, yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent("select * from xml where url='http://www.goear.com/playersong/" + audioId + "' and itemPath='playlists.playlist.track'") + '&format=json';
+
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var result = JSON.parse(xmlhttp.responseText).query.results.track;
+            callback(result);
         }
-    });
+    };
+    xmlhttp.open("GET", yql, true);
+    xmlhttp.send();
+
 }
 
-function processData(rawData) {
-    "use strict";
-    var audioData = {}, fragmentos, pathfrag, direcc, title, artist;
-
-    fragmentos = rawData.split('path="');
-    pathfrag = fragmentos[1].split('"');
-    direcc = pathfrag[0];
-
-    fragmentos = rawData.split('title="');
-    pathfrag = fragmentos[1].split('"');
-    title = pathfrag[0];
-
-    fragmentos = rawData.split('artist="');
-    pathfrag = fragmentos[1].split('"');
-    artist = pathfrag[0];
-
-    audioData.url = direcc;
-    audioData.title = title;
-    audioData.artist = artist;
-    return audioData;
-}
 
 function createButtonUI(audioData) {
-    "use strict";
-    document.getElementById('report_action').parentNode.parentNode.removeChild(document.getElementById('report_action').parentNode);
+    'use strict';
 
-    var dwnbox, dwnbutton, texto, dwnlink, position;
+    var dwnbox, dwnlink, position;
 
     dwnbox = document.createElement('li');
-    dwnbutton = document.createElement('button');
-    dwnbutton.setAttribute('type', 'button');
-    dwnbutton.setAttribute('class', 'btn_action');
-    dwnbutton.setAttribute('id', 'share_action');
-
-    texto = document.createTextNode('Descargar');
-    dwnbutton.appendChild(texto);
 
     dwnlink = document.createElement('a');
-    dwnlink.setAttribute('href', audioData.url);
+    dwnlink.textContent = 'Descargar';
+    dwnlink.setAttribute('href', audioData.href);
     dwnlink.setAttribute('title', 'Haz click con el segundo boton y elige guardar como...');
-    dwnlink.setAttribute('class', 'dragout');
+    dwnlink.style.backgroundPosition = '10px -342px';
+    dwnlink.setAttribute('class', 'radius_3 dragout');
     dwnlink.setAttribute('draggable', 'true');
-    dwnlink.setAttribute('data-downloadurl', 'audio/mpeg:' + audioData.artist + ' - ' + audioData.title + '.mp3:' + audioData.url);
+    dwnlink.setAttribute('data-downloadurl', 'audio/mpeg:' + audioData.title + '.mp3:' + audioData.href);
 
     dwnlink.onclick = function (e) {
         e.preventDefault();
@@ -98,7 +81,6 @@ function createButtonUI(audioData) {
     };
 
     dwnbox.appendChild(dwnlink);
-    dwnlink.appendChild(dwnbutton);
 
     position = document.getElementById('actions');
     position.insertBefore(dwnbox, position.firstChild);
@@ -109,10 +91,9 @@ function createButtonUI(audioData) {
 }
 
 (function () {
-    "use strict";
+    'use strict';
     var audioId = getId();
-    getAudioData(audioId, function (data) {
-        var audioInfo = processData(data);
+    getAudioData(audioId, function (audioInfo) {
         createButtonUI(audioInfo);
     });
 }());
