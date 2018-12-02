0417b765845cab1513dc9d1962767503570ef6a2
spun <neoappengine@gmail.com>
2018-12-02 12:52:10 +0100

    Prototipo de descarga de fichero m3u para mixer

--- src/mixer/mixer.js
+++ src/mixer/mixer.js
@@ -1,3 +1,38 @@
+function getManifestUrl() {
+  const results = window.performance
+    .getEntriesByType('resource')
+    // .filter(resource => resource.initiatorType === 'xmlhttprequest') // changes between browsers
+    .filter(request => request.name.endsWith('manifest.m3u8'));
+
+  return results[results.length - 1].name;
+}
+
+function downloadAsFile(filename, text) {
+  // Create link element
+  const element = document.createElement('a');
+  element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`);
+  element.setAttribute('download', filename);
+  // Add to dom
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  // trigger click event
+  element.click();
+  // remove from dom
+  document.body.removeChild(element);
+}
+
+function getManifestText(manifestUrl) {
+  const baseUrl = manifestUrl.replace('manifest.m3u8', '');
+
+  fetch(manifestUrl)
+    .then(response => response.text())
+    .then((text) => {
+      const result = text.replace(/source/g, `${baseUrl}source`);
+      downloadAsFile('result.m3u8', result);
+    });
+}
+
 export default () => {
-  console.log('From mixer');
+  const manifestUrl = getManifestUrl();
+  getManifestText(manifestUrl);
 };
