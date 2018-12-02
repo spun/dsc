294b49f7a1a35ce1a4e8c20260593388ee3e753b
spun <neoappengine@gmail.com>
2018-12-02 13:46:20 +0100

    Cambia la url de producción a la dirección correcta.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,12 @@
-const merge = require('webpack-merge');
-const webpack = require('webpack');
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge");
+const webpack = require("webpack");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.APP_PATH': JSON.stringify('https://spun.github.io/dsc/'),
-    }),
-  ],
+      "process.env.APP_PATH": JSON.stringify("https://spun.github.io/dsc/dist/")
+    })
+  ]
 });
