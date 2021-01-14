b78aba3606cf835c53b8ca73d35577befeb48684
spun <spun@users.noreply.github.com>
2021-01-14 21:14:55 +0000

    deploy: b9cf61d11da36d174550dc256debf142b777daa2

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,2 @@
-(()=>{let e;switch(document.domain){case"www.youtube.com":e=import("./youtube-8421813f.js");break;case"twitch.tv":e=import("./twitch-b74317e6.js");break;case"mixer.com":e=import("./mixer-7559194e.js");break;default:console.log(`Unknown domain: ${document.domain}`)}e&&e.then((e=>{e.default()}))})();
+(()=>{let e;switch(document.domain){case"www.youtube.com":e=import("./youtube-8421813f.js");break;case"twitch.tv":e=import("./twitch-bf213c79.js");break;case"mixer.com":e=import("./mixer-7559194e.js");break;default:console.log(`Unknown domain: ${document.domain}`)}e&&e.then((e=>{e.default()}))})();
 //# sourceMappingURL=main.js.map
