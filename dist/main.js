4d6bc7dc0643ffd8137b9d6a83e4a05b2f7c6b4d
spun <spun@users.noreply.github.com>
2021-11-04 19:09:10 +0000

    deploy: 1c4c237c965764d6de0cd87d541b229e628ffed9

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,2 @@
-(()=>{let e;switch(document.domain){case"www.youtube.com":e=import("./youtube-872a9e51.js");break;case"twitch.tv":e=import("./twitch-bf213c79.js");break;case"mixer.com":e=import("./mixer-7559194e.js");break;default:console.log(`Unknown domain: ${document.domain}`)}e&&e.then((e=>{e.default()}))})();
+!function(){var o;switch(document.domain){case"www.youtube.com":o=import("./youtube-f9ad9f02.js");break;case"twitch.tv":o=import("./twitch-4da715f8.js");break;case"mixer.com":o=import("./mixer-ca4b5d7d.js");break;default:console.log("Unknown domain: "+document.domain)}o&&o.then((function(o){o.default()}))}();
 //# sourceMappingURL=main.js.map
