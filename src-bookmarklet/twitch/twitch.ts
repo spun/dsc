1d0c90f1d0dbab38763928a7ac22533922083e03
spundev <spundev@gmail.com>
2025-05-17 14:31:09 +0000

    Modify how VOD m3u8 files are downloaded - This should fix the issue with VODs from ongoing streams not playing/downloading from the beginning.

--- src-bookmarklet/twitch/twitch.ts
+++ src-bookmarklet/twitch/twitch.ts
@@ -1,4 +1,4 @@
-import { getLiveAccessToken, getVideoAccessToken } from './utils/accessUtils';
+import { getLiveAccessToken, getBaseDvrUrl } from './utils/accessUtils';
 
 interface CommonOptions {
   headers: Record<string, string>;
@@ -72,14 +72,101 @@ function getVodId() {
 }
 
 /**
- * Get the manifest (m3u8 file) of a live channel
+ * Check URL against known formats and select the one with the best quality
+ * @param baseUrl 
+ * @returns A Promise with the best m3u8 url or undefined if we where unable to find one.
  */
-async function getManigestVideoUrl(videoId: string, clientId: string) {
-  const accessToken = await getVideoAccessToken(clientId, videoId);
-  // Form url
-  const { signature } = accessToken;
-  const encodedToken = encodeURIComponent(accessToken.token);
-  return `https://usher.ttvnw.net/vod/${videoId}.m3u8?sig=${signature}&token=${encodedToken}`;
+async function getBestM3u8FromSupportedFormats(baseUrl: string): Promise<string | undefined> {
+
+  // NOTE: For "Source" quality it will normally use "chunked" instead of a format string. 
+  //  Since we are not checking against "chunked", we will probably always get the second-best quality.
+  // TODO: We could display a quality selector
+  const formats = [
+    "1080p60",  // HighHighFPS
+    "1080p30",  // High
+    "720p60",   // MediumHighFPS
+    "720p30",   // Medium
+    "360p30",   // Low
+    "160p30"    // VeryLow
+  ]
+
+  // Check all known formats and stop when we find one that exists.
+  for (const format of formats) {
+    const formatUrl = `${baseUrl}/${format}/index-dvr.m3u8`
+    // We use a full fetch instead of just HEAD because it creates CORS issues.
+    const response = await fetch(formatUrl);
+    if (response.ok) {
+      return formatUrl
+    }
+  }
+
+  return undefined
+}
+
+/**
+ * Default dvr m3u8 files use relative paths for its .ts files. We need to transform it
+ * to use full paths before it can be downloaded.
+ * @param url The m3u8 file url.
+ * @returns A Promise with the tranformend m3u8 content as a string.
+ */
+async function transformRelativeM3u8ToFullPath(url: string): Promise<string> {
+
+  // Remove last piece from the url to get the base url that will be added to the .ts entries
+  const baseUrl = url.replace(/\/[^/]*$/, '/');
+
+  // Fetch the m3u8 content
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw new Error("Unable to fetch manifest")
+  }
+  const m3u8Content = await response.text()
+
+  // For each line, replace relative .ts entries with full path URLs
+  const m3u8Pieces = m3u8Content
+    .split('\n')
+    .map(line => {
+      // Match lines that look like a segment file, e.g., "23.ts"
+      if (/^\d+\.ts$/.test(line.trim())) {
+        return baseUrl.replace(/\/$/, '') + '/' + line.trim();
+      }
+      return line;
+    })
+
+  // Make sure our m3u8 file ends with #EXT-X-ENDLIST
+  // If we are working with a VOD from an ongoing livestream, this line won't exist.
+  // To ensure the generated m3u8 plays/downloads from the beginning, check if 
+  // the line exists and add it if it doesn't.
+  let lastNonEmptyIndex: number | undefined = undefined;
+  for (let i = m3u8Pieces.length - 1; i >= 0; i--) {
+    if (m3u8Pieces[i] !== "") {
+      lastNonEmptyIndex = i;
+      break;
+    }
+  }
+
+  if (lastNonEmptyIndex && m3u8Pieces[lastNonEmptyIndex] !== "#EXT-X-ENDLIST") {
+    m3u8Pieces.splice(lastNonEmptyIndex + 1, 0, "#EXT-X-ENDLIST")
+  }
+
+  return m3u8Pieces.join('\n')
+
+}
+
+/**
+ * Download some string content as a m3u8 file
+ * @param filename The name of the new file.
+ * @param content The content of the new file.
+ */
+function downloadM3u8File(filename: string, content: string) {
+  const blob = new Blob([content], { type: "application/vnd.apple.mpegurl" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
 }
 
 async function main(): Promise<void> {
@@ -94,8 +181,14 @@ async function main(): Promise<void> {
     const vodId = getVodId();
     if (vodId) {
       const clientId = getClientId();
-      const videoManifestUrl = await getManigestVideoUrl(vodId, clientId);
-      window.location.href = videoManifestUrl;
+      const baseDvrUrl = await getBaseDvrUrl(clientId, vodId)
+      const bestAvailableM3u8 = await getBestM3u8FromSupportedFormats(baseDvrUrl)
+      if (bestAvailableM3u8) {
+        const m3u8Content = await transformRelativeM3u8ToFullPath(bestAvailableM3u8)
+        downloadM3u8File(`${vodId}.m3u8`, m3u8Content)
+      } else {
+        console.warn("Unable to find a valid format")
+      }
     }
   }
 }
--- src-bookmarklet/twitch/utils/accessUtils.ts
+++ src-bookmarklet/twitch/utils/accessUtils.ts
@@ -23,7 +23,7 @@ interface AccessToken {
  * @param channelName If the petition is for a live channel, this has to be the channel name.
  * @param videoId If the petition is for a VOD, this has to be the id of the video.
  */
-async function getAccessTokenResponse(clientId: string, isLive: boolean, channelName = '', videoId = '') : Promise<ApiResponse> {
+async function getAccessTokenResponse(clientId: string, isLive: boolean, channelName = '', videoId = ''): Promise<ApiResponse> {
   const gqlTemplate = `
     query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {
       streamPlaybackAccessToken(channelName: $login, params: {platform: "web", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isLive) {
@@ -68,7 +68,7 @@ async function getAccessTokenResponse(clientId: string, isLive: boolean, channel
 export async function getLiveAccessToken(
   clientId: string,
   channelName: string,
-) : Promise<AccessToken> {
+): Promise<AccessToken> {
   const dataJson = await getAccessTokenResponse(clientId, true, channelName, undefined);
   const { value: token, signature } = dataJson.data.streamPlaybackAccessToken;
   return { token, signature };
@@ -77,8 +77,76 @@ export async function getLiveAccessToken(
 export async function getVideoAccessToken(
   clientId: string,
   videoId: string,
-) : Promise<AccessToken> {
+): Promise<AccessToken> {
   const dataJson = await getAccessTokenResponse(clientId, false, undefined, videoId);
   const { value: token, signature } = dataJson.data.videoPlaybackAccessToken;
   return { token, signature };
+}
+
+/**
+ * Get the shared structure for the different index-dvr.m3u8 URLs. This can be used to generate valid index-dvr.m3u8 URLs.
+
+ * @param videoId The id of the video
+ * @returns A string with the part of the URL that is shared between dvr m3u8 files.
+ */
+export async function getBaseDvrUrl(clientId: string, videoId: string) {
+
+  // This is stupid, but we can't access the base m3u8 content if Firefox's Enhanced Tracking Protection is enabled. 
+  // This means that we can't read the available index-dvr.m3u8 URLs directly, we need to "guess" them by requesting
+  // other resources that share the same base URL. In this case, we are using the "storyboards" URL.
+
+  // Body of the gql query we will use to get the "storyboards" URL
+  const requestBody =
+  {
+    "operationName": "VideoPlayer_VODSeekbarPreviewVideo",
+    "variables": {
+      "includePrivate": false,
+      "videoID": videoId
+    },
+    "extensions": {
+      "persistedQuery": {
+        "version": 1,
+        "sha256Hash": "07e99e4d56c5a7c67117a154777b0baf85a5ffefa393b213f4bc712ccaf85dd6"
+      }
+    }
+  }
+
+  // Get api response
+  const response = await fetch("https://gql.twitch.tv/gql", {
+    "headers": {
+      "client-id": clientId,
+    },
+    "body": JSON.stringify(requestBody),
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "omit"
+  })
+
+  if (!response.ok) {
+    throw new Error("Unable to fetch manifest")
+  }
+
+  const responseJson = await response.json()
+
+  // Extract storyboads URL from response
+  const fullStoryboardsUrl = responseJson["data"]["video"]["seekPreviewsURL"]
+
+  // Split the pathname of the url
+  const url = new URL(fullStoryboardsUrl);
+  const pathnamePieces = url.pathname.split("/");
+
+  // Check if the current url has the expected format (ends with /storyboards/<videoId>-info.json)
+  const expectedFile = `${videoId}-info.json`;
+  if (pathnamePieces.at(-1) !== expectedFile || pathnamePieces.at(-2) !== "storyboards") {
+    throw new Error(`Storyboards URL does not have the expected format: ${fullStoryboardsUrl}`);
+  }
+
+  // Take everything before "/storyboards"
+  const beforeStoryboards = pathnamePieces
+    .slice(0, pathnamePieces.indexOf("storyboards"))
+    .join("/");
+
+  // Rebuild full URL
+  url.pathname = beforeStoryboards
+  return url.toString();
 }
